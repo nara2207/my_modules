1ff35eab90536d70d07edde1e094dc31d5d5db7b
Nara Begzhan <gulnar2207@gmail.com>
2023-11-20 21:31:30 -0500

    change vpc

--- vpc/outputs.tf
+++ vpc/outputs.tf
@@ -3,7 +3,7 @@ output "id" {
 }
 
 output "igw_id" {
-  value = var.create_attach_igw ? aws_internet_gateway[0].id : null
+  value = var.create_attach_igw ? aws_internet_gateway.igw[0].id : null
   #value = try(aws_internet_gateway.igw[0.id, null])
 
 }
